706d15e2ec5761ea9d4612fb1a66edec1891a170
hydrobot004 <91904299+hydrobot004@users.noreply.github.com>
2022-12-31 21:21:33 -0500

    Add files via upload

--- cobalt-sept-2022.py
+++ cobalt-sept-2022.py
@@ -13,7 +13,7 @@ sheet_name = 'DATA'
 
 df = pd.read_excel(excel_file,
                     sheet_name=sheet_name,
-                    usecols='B',
+                    usecols='A:C',
                     header=0)
             
 df_participants = pd.read_excel(excel_file,
